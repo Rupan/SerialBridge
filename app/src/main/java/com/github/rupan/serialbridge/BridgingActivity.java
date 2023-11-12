81e7607b170c6c2bfdfa1a0f3bf272f16929378d
Michael Mohr <akihana@gmail.com>
2023-11-12 15:55:56 -0800

    Implement UI, first try

--- app/src/main/java/com/github/rupan/serialbridge/BridgingActivity.java
+++ app/src/main/java/com/github/rupan/serialbridge/BridgingActivity.java
@@ -3,6 +3,7 @@ package com.github.rupan.serialbridge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BridgingActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class BridgingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bridging);
+        // final TextView t = findViewById(R.id.network_parameters_header);
+        // t.setText("some static text");
+        // t.setText(R.string.user_greeting);
     }
 }
