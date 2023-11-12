7b558432dad55b2c0ad4f68307f95c5f364cc8f5
Michael Mohr <akihana@gmail.com>
2023-11-12 14:06:47 -0800

    Add the main Bridging activity using constraint layout

---
+++ app/src/main/java/com/github/rupan/serialbridge/BridgingActivity.java
@@ -0,0 +1,14 @@
+package com.github.rupan.serialbridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BridgingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bridging);
+    }
+}
