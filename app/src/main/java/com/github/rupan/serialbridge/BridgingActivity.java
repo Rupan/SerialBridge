35fbf4782a712f80caf18c6826617f592f8fc8aa
Michael Mohr <akihana@gmail.com>
2023-11-12 19:13:45 -0800

    Add IP address drop-down

--- app/src/main/java/com/github/rupan/serialbridge/BridgingActivity.java
+++ app/src/main/java/com/github/rupan/serialbridge/BridgingActivity.java
@@ -1,8 +1,11 @@
 package com.github.rupan.serialbridge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
 public class BridgingActivity extends AppCompatActivity {
@@ -14,5 +17,13 @@ public class BridgingActivity extends AppCompatActivity {
         // final TextView t = findViewById(R.id.network_parameters_header);
         // t.setText("some static text");
         // t.setText(R.string.user_greeting);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AppCompatAutoCompleteTextView textView = findViewById(R.id.ip_address_dropdown);
+        textView.setAdapter(adapter);
     }
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
 }
